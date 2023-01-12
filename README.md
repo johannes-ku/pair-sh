02aea0c279232648e40fcb087ccf2416f6076990
Ellen Ennok, Johannes Kümmel <ellen@progmatic.ee johannes@progmatic.ee>
2023-01-12 12:19:24 +0200

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# pair-sh
+
+A simple script for setting git `user.name` and `user.email` faster when doing pair programming.
+
+## Setup
+
+1. Host `people.json` (see `people.json.example` for example) somewhere
+2. Change the url in `pair.sh`
+
+## Usage
+
+`./pair.sh name1 name2`
---
+++ pair.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+json=`curl -s http://127.0.0.1:8080/people.json`
+if [ $# -eq 0 ];
+then
+    echo "Available profiles:"
+    echo $json | jq -r '.people | keys | .[]'
+else
+    combined_name=""
+    combined_email=""
+    for i in "$@"; do
+        if echo $json > jq -r '.people | has("'$i'")';
+        then
+            current_name=`jq -r '.people.'$i'.name' <(echo "$json")`
+            current_email=`jq -r '.people.'$i'.email' <(echo "$json")`
+            echo "CURRENT NAME: $current_name"
+            echo "CURRENT EMAIL: $current_email"
+            if [ "$combined_name" == "" ];
+            then
+                combined_name=$current_name
+                combined_email=$current_email
+            else
+                combined_name="$combined_name, $current_name"
+                combined_email="$combined_email $current_email"
+            fi
+        else
+            echo "Profile for $i not found"
+            exit 1
+        fi
+    done
+    echo "[INFO] Setting Git global user name to: $combined_name"
+    #git config --global user.name "$combined_name"
+    echo "[INFO] Setting Git global user email to: $combined_email"
+    #git config --global --replace-all user.email "$combined_email"
+fi
